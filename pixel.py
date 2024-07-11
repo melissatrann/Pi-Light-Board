77d56f4cd0c685b23aed26cef61aae7ca604825f
jly02 <95561288+jly02@users.noreply.github.com>
2024-07-11 13:46:40 -0700

    Update pixel.py

--- pixel.py
+++ pixel.py
@@ -3,25 +3,27 @@ class Pixel():
         self.color = grb
         self.brightness = how_bright
 
-    def brightness(value):
+    def brightness(self, value):
         self.brightness = value
 
+    def set_color(self, grb):
+        self.color = grb
+
     def __repr__(self):
-        return f"({self.grb}, {self.brightness})"
-    
+        return f"({self.color}, {self.brightness})"  
         
 class PixelArray():
     def __init__(self, num_pixels):
         self.num_pixels = num_pixels
-        self.pixels = [Pixel() for _ in range(num_pixels)]
+        self.pixels = [Pixel((255, 255, 255), 1.0) for _ in range(num_pixels)]
         
     def set_pixel(self, index, grb, brightness=1.0):
         if 0 <= index < self.num_pixels:
             self.pixels[index].set_color(grb)
             self.pixels[index].set_brightness(brightness)
 
-    def fill(grb):
-        list(map(lambda c: c.color = grb, self.pixels))
+    def fill(self, grb):
+        list(map(lambda c: c.set_color(grb), self.pixels))
 
     def __repr__(self):
-        return ' - '.join(self.pixels)
+        return str(self.pixels)
