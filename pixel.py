f8f003c273f49bdbb6c730662c83d48922375781
jly02 <95561288+jly02@users.noreply.github.com>
2024-07-11 13:33:42 -0700

    Update pixel.py

--- pixel.py
+++ pixel.py
@@ -5,4 +5,16 @@ class Pixel():
 
     def brightness(value):
         self.brightness = value
+
+    def __repr__(self):
+        return f"({self.grb}, {self.brightness})"
         
+class PixelArray():
+    def __init__(self, num_pixels):
+        self.num_pixels = num_pixels
+        self.pixels = [Pixel() for _ in range(num_pixels)]
+
+    def fill(grb) 
+
+    def __repr__(self):
+        return ' - '.join(self.pixels)
