1b5e5c5c59519781c4758afe1aafea7436c835e3
jly02 <95561288+jly02@users.noreply.github.com>
2024-07-11 13:11:37 -0700

    Create pixel.py

---
+++ pixel.py
@@ -0,0 +1,8 @@
+class Pixel():
+    def __init__(self, grb, how_bright):
+        self.color = grb
+        self.brightness = how_bright
+
+    def brightness(value):
+        self.brightness = value
+        
